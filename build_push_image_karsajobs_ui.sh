a26921711b28bf01cc34634b89b6697e43cf53d5
ekowinardi <bagaswinardi@gmail.com>
2023-12-20 00:22:25 +0700

    Add command

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-# build docker image dari berkas dockerfile
+# build docker image dari berkas dockerfile (karsajobs-ui)
 docker build -t ghcr.io/ekowinardi/karsajobs-ui:latest .
 # login ke github pacakge
 echo $CR_PAT | docker login -u ghcr.io/ekowinardi --password-stdin
